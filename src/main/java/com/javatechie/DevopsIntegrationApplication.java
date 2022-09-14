e6a8f8b61c9862b4984c10e63e452961f9d518f3
shijupulickal <103875011+shijupulickal@users.noreply.github.com>
2022-09-14 13:41:18 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to shiju ";
+		return "WELCOME TO DEVOPS ";
 	}
 
 	public static void main(String[] args) {
